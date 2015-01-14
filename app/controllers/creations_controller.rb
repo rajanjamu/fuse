7a73797b08b3df783b5ffa9e9512e9c1ca77eb7e
Rajan Gupta <rajanjamu@gmail.com>
2015-01-14 13:53:48 +0530

    commit

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -10,6 +10,19 @@ class CreationsController < ApplicationController
     @users = User.all
     @categories = Category.all
     @creation = Creation.new
+    @category_names = Category.pluck(:name)
+    @user_names = User.pluck(:name)
+  end
+
+  def create
+    parameters = params[:creation]
+    @category_id = Category.find_by(name: parameters["category_id"]).id
+    @user_id = User.find_by(name: parameters["user_id"]).id
+    @creation = Creation.new(creation_params)
+    @creation.category_id = @category_id
+    @creation.user_id = @user_id
+    @creation.save
+    redirect_to @creation
   end
 
   def edit
